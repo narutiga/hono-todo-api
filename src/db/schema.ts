c4d5552fad2016b1d44240ea684bd8c22ede54bb
narutiga <100295602+narutiga@users.noreply.github.com>
2023-05-04 14:01:01 +0900

    create todos table.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,6 +1,20 @@
-import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { sql } from "drizzle-orm";
 
 export const users = sqliteTable("users", {
   id: text("id").primaryKey().notNull(),
   name: text("name").notNull(),
+});
+
+export const todos = sqliteTable("todos", {
+  id: text("id").primaryKey().notNull(),
+  title: text("title").notNull(),
+  completed: integer("completed").notNull().default(0),
+  createdAt: text("createdAt")
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  updatedAt: text("updatedAt")
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  userId: text("userId").references(() => users.id),
 });
