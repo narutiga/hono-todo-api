e8ecd28749766a65c707bd42466fc38d5dbf573c
narutiga <100295602+narutiga@users.noreply.github.com>
2023-05-06 17:14:35 +0900

    🐛 configure CORS setting.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import { Hono } from "hono";
-import { todos } from "./todos";
+import { cors } from "hono/cors";
 import { Env } from "./bindings";
+import { todos } from "./api/todos";
 
 const app = new Hono<{ Bindings: Env }>();
+
+app.use(
+  "/api/*",
+  cors({
+    origin: "http://localhost:3000",
+    maxAge: 600,
+    credentials: true,
+  })
+);
 
 app.route("/api/todos", todos);
 
